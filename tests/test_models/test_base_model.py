169bff3993504ae15cd63eb4085e3759157cbcd8
Iyanu456 <oyerindei13@gmail.com>
2023-02-12 18:34:51 +0100

    default message

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,8 +12,10 @@ class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         self.basemodel = BaseModel()
+        self.basemodel_1 = BaseModel()
 
     def test_doc(self):
+        """test doc"""
         self.assertIsNotNone(BaseModel.__doc__)
 
     def test_save_BaseModel(self):
@@ -22,7 +24,25 @@ class TestBaseModel(unittest.TestCase):
         self.basemodel.save()
         self.assertEqual(self.basemodel.created_at, self.basemodel.updated_at)
 
+    def test_basemodel_id(self):
+        """test basemodel id"""
+        self.assertNotEqual(self.basemodel.id, self.basemodel_1.id)
+
+    def test_basemodel_name(self):
+        """test basemodel name"""
+        self.basemodel.name = "model_1"
+        self.basemodel_1.name = "model_2"
+        self.assertTrue(hasattr(self.basemodel, "name"))
+        self.assertTrue(hasattr(self.basemodel_1, "name"))
+        self.assertNotEqual(self.basemodel.name, self.basemodel_1.name)
+
+    def test_instance(self):
+        """test instance"""
+        self.assertTrue(isinstance(self.basemodel, BaseModel))
+        self.assertTrue(isinstance(self.basemodel_1, BaseModel))
+
     def test_kwarg(self):
+        """test kwqrg"""
         basemodel = BaseModel(name="base")
         self.assertEqual(type(basemodel).__name__, "BaseModel")
         self.assertFalse(hasattr(basemodel, "id"))
